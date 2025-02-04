54aafcc7985a5a982500b1d9d5e727972abf8c7e
Noelia Camelia <noeliacamelia@gmail.com>
2025-02-04 10:04:16 +0100

    Get routes

---
+++ src/controllers/getContactControler.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const getContactControler = (req, res) => {
+  res.send("You can see us in our office.");
+};
+
+export default getContactControler;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,17 @@
 import express from "express";
+import getHomeController from "../controllers/getHomeController.js";
+import getContactController from "../controllers/getContactControler.js";
+import getAppointmentController from "../controllers/getAppointmentsController.js";
+import getAdminController from "../controllers/getAdminController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+router.get("/", getHomeController);
 
-router.get("/contact", (req, res) => {
-  res.send("You can see us in our office.");
-});
+router.get("/contact", getContactController);
 
-router.get("/appointments", (req, res) => {
-  res.send("You can take an appointments with us.");
-});
+router.get("/appointments", getAppointmentController);
 
-router.get("/admin", (req, res) => {
-  res.send("Route of admins.");
-});
+router.get("/admin", getAdminController);
 
 export default router;
