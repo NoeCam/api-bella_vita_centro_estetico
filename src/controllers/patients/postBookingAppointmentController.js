6872e8dacca7df5125bf0b92f689bd4eb29ecc9c
Noelia Camelia <noeliacamelia@gmail.com>
2025-03-01 20:52:38 +0100

    corrections in booking controller and service

--- src/controllers/patients/postBookingAppointmentController.js
+++ src/controllers/patients/postBookingAppointmentController.js
@@ -2,22 +2,21 @@ import insertBookingService from "../../services/insertBookingService.js";
 
 const postBookingAppointmentController = async (req, res, next) => {
   try {
-    const { first_name, last_name, email, celphone, date, startTime, endTime } =
+    const { first_name, last_name, email, celphone, date, startTime } =
       req.body;
 
     const treatmentId = req.query.treatmentId;
 
     const adminId = 1;
     await insertBookingService(
+      treatmentId,
+      date,
+      startTime,
       first_name,
       last_name,
       email,
       celphone,
-      treatmentId,
-      adminId,
-      date,
-      startTime,
-      endTime
+      adminId
     );
 
     res.send({
